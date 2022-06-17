ae7d56cb5e7719906082c2447f17c81aab8b1be0
Omar OUKIL <omaroukil31@gmail.com>
2022-06-17 23:25:26 +0100

    adding the route domain

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +28,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["omaroukil.eu-gb.cf.appdomain.cloud"]
+ALLOWED_HOSTS = ['omaroukil.eu-gb.cf.appdomain.cloud', 'localhost']
 
 
 # Application definition
